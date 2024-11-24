e0bc66452b78a9cbc4982253f2274035ca1406dd
lyh5426 <lyh5426@naver.com>
2024-11-24 20:54:06 +0900

    feat - 앱 진입시 환율 정보 가져오기 구현

--- app/src/main/java/com/villains/fool/presentation/main/MainActivity.kt
+++ app/src/main/java/com/villains/fool/presentation/main/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.villains.fool.presentation.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 import com.villains.fool.R
 import com.villains.fool.databinding.ActivityMainBinding
 import com.villains.fool.presentation.main.home.Home
@@ -11,17 +15,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    val model: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
     }
 
     override fun onResume() {
         super.onResume()
 
+        model.reqExchageRate()
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.mainFragmentContainer, Home()).commit()
--- app/src/main/java/com/villains/fool/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/villains/fool/presentation/main/MainViewModel.kt
@@ -1,7 +1,49 @@
 package com.villains.fool.presentation.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.villains.fool.Application
+import com.villains.fool.domain.DataRepositorySource
+import com.villains.fool.domain.di.dto.remote.ExchangeRateVo
+import com.villains.fool.domain.di.dto.remote.OnResponse
+import com.villains.fool.domain.di.dto.remote.Response
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel: ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repo: DataRepositorySource
+): ViewModel() {
+    fun reqExchageRate() {
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.getExchangeRate("20241121").collectLatest {
+                when (it) {
+                    is Response.Success -> {
+                        Application.prefs.exchangeRate.clear()
+                        val userListType = object : TypeToken<OnResponse<List<ExchangeRateVo>>>() {}.type
+                        val users: OnResponse<List<ExchangeRateVo>> = Gson().fromJson(it.response as String, userListType)
 
+                        users.data!!.forEach {
+                            val ctrCode = Application.prefs.currencyToCountry[it.ctrDivCd]?: "unknown"
+                            Application.prefs.exchangeRate.set(ctrCode, it.exgrBaseRate)
+
+                            Log.d("Tag", "${it.ctrDivCd} ${ctrCode}   ${it.exgrBaseRate}")
+                        }
+
+                        Log.d("Tag", "${it.response}")
+                    }
+
+                    is Response.Fail -> {
+                        Log.d("Tag", "${it.err}")
+                    }
+                }
+            }
+        }
+    }
 }
